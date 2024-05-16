db126be4d2641fb47e6be1e954aa9867d3e5a419
sluggishTurtlee <164842777+sluggishTurtlee@users.noreply.github.com>
2024-05-16 10:39:06 +0900

    Create 169. Majority Element.py

---
+++ 169. Majority Element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        num_list=dict()
+
+        for num in nums:
+            if not num in num_list:
+                num_list[num]=1
+            else:
+                num_list[num]+=1
+                
+        return max(num_list, key=num_list.get)
