3ef080bbfb535924bcc072105f26e95d26be1cbb
sluggishTurtlee <164842777+sluggishTurtlee@users.noreply.github.com>
2024-05-16 10:33:41 +0900

    Create 136. Single Number.py

---
+++ 136. Single Number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        l = len(nums)
+
+        for i in range(l):
+            if nums.count(nums[i]) == 1:
+                return nums[i]
+                break
+            
+            else: continue
