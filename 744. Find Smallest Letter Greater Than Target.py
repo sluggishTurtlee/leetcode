ef9920be7d7725428c9c2207c21480f9c546a53a
sluggishTurtlee <164842777+sluggishTurtlee@users.noreply.github.com>
2024-06-14 00:06:56 +0900

    Create  744. Find Smallest Letter Greater Than Target.py

---
+++ 744. Find Smallest Letter Greater Than Target.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def nextGreatestLetter(self, letters, target):
+        """
+        :type letters: List[str]
+        :type target: str
+        :rtype: str
+        """
+        return letters[bisect.bisect(letters, target) % len(letters)]        
