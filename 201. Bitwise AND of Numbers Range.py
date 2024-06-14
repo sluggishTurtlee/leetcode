1822e0b339acde1b192dba55e5f9cbe94def56fc
sluggishTurtlee <164842777+sluggishTurtlee@users.noreply.github.com>
2024-06-15 00:57:54 +0900

    Create  201. Bitwise AND of Numbers Range.py

---
+++ 201. Bitwise AND of Numbers Range.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def rangeBitwiseAnd(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: int
+        """
+    def rangeBitwiseAnd(self, m, n):
+        i = 0
+        while m != n:
+            m >>= 1
+            n >>= 1
+            i += 1
+        return m << i        
