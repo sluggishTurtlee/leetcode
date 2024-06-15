8b1031ed719d2191e8fb7a8f8a5f3d4a378ea193
sluggishTurtlee <164842777+sluggishTurtlee@users.noreply.github.com>
2024-06-15 20:38:13 +0900

    Create  263. Ugly Number.py

---
+++ 263. Ugly Number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        while num>1:
+            if num%2!=0 and num%3!=0 and num%5!=0: return False
+            else: num/=[i for i in (2,3,5) if num%i==0][-1]
+        return num==1   
