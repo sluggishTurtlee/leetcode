63bd2075084332b7a4ed0a22f6c8ad8cdb454405
sluggishTurtlee <164842777+sluggishTurtlee@users.noreply.github.com>
2024-06-15 20:42:40 +0900

    Create  242. Valid Anagram.py

---
+++ 242. Valid Anagram.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+
+        #return sum([ord(i) for i in s])==sum([ord(j) for j in t]) and set(s)==set(t)
+        return sorted(s)==sorted(t)        
