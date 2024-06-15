3ae5f6b073926a6a99b92cd8560071797caf7a9e
sluggishTurtlee <164842777+sluggishTurtlee@users.noreply.github.com>
2024-06-15 22:29:04 +0900

    Create 551. Student Attendance Record I.py

---
+++ 551. Student Attendance Record I.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def checkRecord(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        return False if "LLL" in s or s.count("A")>1 else True        
